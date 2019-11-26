f09ec6e8fbd2f30a62687cd1c0d51b4adf9e8abb
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2019-11-26 14:09:23 +0300

    --- work in progress

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/JvmArgumentNullabilityAssertionsLowering.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/JvmArgumentNullabilityAssertionsLowering.kt
@@ -8,22 +8,27 @@ package org.jetbrains.kotlin.backend.jvm.lower
 import org.jetbrains.kotlin.backend.common.FileLoweringPass
 import org.jetbrains.kotlin.backend.common.phaser.makeIrFilePhase
 import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
+import org.jetbrains.kotlin.config.ApiVersion
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.declarations.IrFile
-import org.jetbrains.kotlin.ir.expressions.IrMemberAccessExpression
-import org.jetbrains.kotlin.ir.expressions.IrTypeOperator
-import org.jetbrains.kotlin.ir.expressions.IrTypeOperatorCall
+import org.jetbrains.kotlin.ir.declarations.IrFunction
+import org.jetbrains.kotlin.ir.expressions.*
+import org.jetbrains.kotlin.ir.util.dump
 import org.jetbrains.kotlin.ir.visitors.IrElementVisitorVoid
 import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
 
 val jvmArgumentNullabilityAssertions =
-    makeIrFilePhase<JvmBackendContext>(
-        { JvmArgumentNullabilityAssertionsLowering() },
+    makeIrFilePhase(
+        ::JvmArgumentNullabilityAssertionsLowering,
         name = "Nullability assertions on arguments",
         description = "Transform nullability assertions on arguments so that they conform to the compiler settings"
     )
 
-class JvmArgumentNullabilityAssertionsLowering : FileLoweringPass, IrElementVisitorVoid {
+class JvmArgumentNullabilityAssertionsLowering(context: JvmBackendContext) :
+    FileLoweringPass, IrElementVisitorVoid {
+
+    private val isWithUnifiedNullChecks =
+        context.state.languageVersionSettings.apiVersion >= ApiVersion.KOTLIN_1_4
 
     override fun lower(irFile: IrFile) {
         irFile.acceptChildrenVoid(this)
@@ -31,6 +36,11 @@ class JvmArgumentNullabilityAssertionsLowering : FileLoweringPass, IrElementVisi
 
     override fun visitElement(element: IrElement) {
         element.acceptChildrenVoid(this)
+    }
+
+    override fun visitFunction(declaration: IrFunction) {
+        println(declaration.dump())
+        super.visitFunction(declaration)
     }
 
     override fun visitMemberAccess(expression: IrMemberAccessExpression) {
@@ -41,9 +51,33 @@ class JvmArgumentNullabilityAssertionsLowering : FileLoweringPass, IrElementVisi
         // NB there are some members in Kotlin built-in classes which are NOT implemented as platform method calls,
         // and thus break this assertion - e.g., 'Array<T>.iterator()' and similar functions.
         // See KT-30908 for more details.
-        val dispatchReceiver = expression.dispatchReceiver
-        if (dispatchReceiver is IrTypeOperatorCall && dispatchReceiver.operator == IrTypeOperator.IMPLICIT_NOTNULL) {
-            expression.dispatchReceiver = dispatchReceiver.argument
+        expression.dispatchReceiver.replaceImplicitNotNullWithArgument {
+            expression.dispatchReceiver = it
+        }
+
+        expression.extensionReceiver.replaceImplicitNotNullWithArgument {
+            if (shouldDropNullabilityAssertionOnExtensionReceiver(expression)) {
+                expression.extensionReceiver = it
+            }
+        }
+    }
+
+    private fun shouldDropNullabilityAssertionOnExtensionReceiver(expression: IrMemberAccessExpression): Boolean {
+        if (!isWithUnifiedNullChecks) {
+            if (expression.origin.isOriginForSpecialOperatorCall) return true
+        }
+
+        return false
+    }
+
+    companion object {
+        val IrStatementOrigin?.isOriginForSpecialOperatorCall
+            get() =
+                this is IrStatementOrigin.COMPONENT_N
+
+        inline fun IrExpression?.replaceImplicitNotNullWithArgument(replacer: (IrExpression) -> Unit) {
+            if (this is IrTypeOperatorCall && this.operator == IrTypeOperator.IMPLICIT_NOTNULL)
+                replacer(argument)
         }
     }
 }
